2da2f1290625a8f89bfc1ea483aef27e3458012a
Danny <danny@kokomo247.com>
2021-10-16 18:28:03 -0700

    half done

--- api.py
+++ api.py
@@ -29,6 +29,10 @@ def sqlite(section="sqlite", key="dbfile", **kwargs):
 def log(name=__name__, **kwargs):
     return logging.getLogger(name)
 
+@hug.directive()
+def timelinesdb(section="timelinesdb", key="dbfile", **kwargs):
+    dbfile = config[section][key]
+    return sqlite_utils.Database(dbfile)
 
 # Routes
 #hug.get(examples='username=Timothy&email=abc@abc.com&password=123')
@@ -59,10 +63,98 @@ def create_users(
     return user
 
 @hug.get("/users/")
-def authenticateUser(request, db:sqlite, username:hug.types.text, password):
+def authenticateUser(db:sqlite, username, password):
 
     users = db["users"]
    
+    return {"users" : users.rows_where("username = ? AND password = ?", [username, password]) }
 
-    return {"users" : users.rows_where("username = \"", username, "\"") }
+#TODO Delete this before submission
+@hug.get("/allusers/")
+def allusers(db:sqlite):
+    return {"Users": db["users"].rows}
 
+@hug.post("/followers/")
+def addFollower(
+    response,
+    follower_id: hug.types.number,
+    following_id: hug.types.number,
+    db: sqlite,
+):
+
+    followers = db["followers"]
+    follower = {
+        "follower_id":follower_id,
+        "following_id":following_id
+    }
+
+    try:
+        followers.insert(follower)
+        follower["id"] = followers.last_pk
+    except Exception as e:
+        response.status = hug.falcon.HTTP_204
+        return {"error": str(e)}
+
+    response.set_header("Location", f"/followers/{follower['id']}")
+    return follower
+
+@hug.get("/followers/")
+def getFollowingId(db:sqlite, response, follower_id:hug.types.number, following_id:hug.types.number):
+
+    followers = db["followers"]
+
+    try:
+        followers.delete_where("follower_id = ? AND following_id = ?", [3,2])
+    except Exception as e:
+        response.status = hug.falcon.HTTP_704
+        
+    # return {"followers" : followers.rows_where("follower_id = ? AND following_id = ?", [follower_id, following_id])}
+    return hug.falcon.HTTP_204
+
+@hug.get("/posts/")
+def getUserTimeline(db:timelinesdb, username):
+    
+    timelines = db["posts"]
+
+    return {"posts" : timelines.rows_where("username = ?", [username])}
+
+@hug.get("/posts/")
+def getPublicTimeline(db:timelinesdb):
+    timelines = db["posts"]
+    return {"posts" : timelines.rows_where()}
+
+@hug.get("/posts/")
+def getHomeTimeline(db:timelinesdb, json = target):
+    
+    timelines = db["posts"]
+    following = []
+    for i in target:
+        following.append(i[following_id])
+
+    return {"posts" : timelines.rows_where()}
+
+
+#TODO FINISH THIS
+@hug.post("/posts/")
+def postTweet(
+    response,
+    username: hug.types.text,
+    text: hug.types.text,
+    db: timelinesdb,
+):
+
+    timelines = db["posts"]
+    post = {
+        "username":username,
+        "text":text
+    }
+
+    try:
+        timelines.insert(text)
+        follower["id"] = followers.last_pk
+    except Exception as e:
+        response.status = hug.falcon.HTTP_204
+        return {"error": str(e)}
+
+    response.set_header("Location", f"/followers/{follower['id']}")
+    return follower
